9f7d0ccd5d5b49e16d5cff8de05a38002803083d
Mithun <mithunm@testvagrant.com>
2026-07-20 16:21:22 +0530

    using testdata

--- test/specs/myDemoAppFrameworktestData.js
+++ test/specs/myDemoAppFrameworktestData.js
@@ -3,13 +3,11 @@ import myDemoAppPaymentPage from "../pageobject/myDemoAppPaymentPage";
 //import myDemoAppLoginPage from "../pageobject/myDemoAppLoginPage";
 import myDemoAppProductPage from "../pageobject/myDemoAppProductPage";
 import myDemoAppPlaceOrderPage from "../pageobject/myDemoAppPlaceOrderPage";
-import checkoutData from "../../testData/checkoutData.json";
-
+import checkoutData from "../../testDataDemoApp/checkout.json";
 describe(' MyDemo App - Checkout Flow', () => {
     it('should login, add product to cart, and complete checkout', async () => {
 
         await myDemoAppProductPage.addProductToCartAndCheckout();
-
         await myDemoAppCheckoutPage.enterCheckoutDetails(checkoutData.checkout.fullName, checkoutData.checkout.address, checkoutData.checkout.city, checkoutData.checkout.state, checkoutData.checkout.postalCode, checkoutData.checkout.country);
        await myDemoAppPaymentPage.enterPaymentDetails(checkoutData.payment.fullName, checkoutData.payment.cardNumber, checkoutData.payment.expirationDate, checkoutData.payment.cvv);
         await myDemoAppPlaceOrderPage.placeOrder();
